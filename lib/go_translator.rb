bb914bf7d1014c56d59f5950fd0de33501e96ad3
Hoang Phan <hoang.phan@eastagile.com>
2015-08-17 18:01:23 +0700

    [b] Fix encoding error with multi word sentences
    v0.2.1

--- lib/go_translator.rb
+++ lib/go_translator.rb
@@ -6,7 +6,7 @@ require "uri"
 module GoTranslator
   def self.translate(text, options = {})
     options = default_options.merge(options)
-    uri = URI.parse("#{GOOGLE_TRANSLATE_URL}/single?client=z&sl=#{options[:from]}&tl=#{options[:to]}&ie=#{options[:in_encoding]}&oe=#{options[:out_encoding]}&dt=t&dt=rm&q=#{text}")
+    uri = URI.parse(URI.encode("#{GOOGLE_TRANSLATE_URL}/single?client=z&sl=#{options[:from]}&tl=#{options[:to]}&ie=#{options[:in_encoding]}&oe=#{options[:out_encoding]}&dt=t&dt=rm&q=#{text}"))
     response = Net::HTTP.get(uri)
     response.nil? || response.empty? ? '' : response.split(SPLITTER)[0].gsub(/[\[|\"]/, '')
   rescue
